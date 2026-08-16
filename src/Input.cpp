5cc6f244d9e0ae52d61274467911d755734be24d
driconmax <driconmax@gmail.com>
2026-08-16 15:58:57 +0900

    Implemented Input

---
+++ src/Input.cpp
@@ -0,0 +1,36 @@
+#include "Input.h"
+
+namespace temgi
+{
+    
+    bool Input::pressed(Button button) const
+    {
+        const auto index = static_cast<std::size_t>(button);
+        return current_[index] && !previous_[index];
+    }
+    
+    bool Input::held(Button button) const
+    {
+        const auto index = static_cast<std::size_t>(button);
+        return current_[index] && previous_[index];
+    }
+    
+    bool Input::released(Button button) const
+    {
+        const auto index = static_cast<std::size_t>(button);
+        return !current_[index] && previous_[index];
+    }
+    
+    void Input::setButton(Button button, bool state)
+    {
+        const auto index = static_cast<std::size_t>(button);
+        current_[index] = state;
+    }
+    
+    void Input::nextFrame()
+    {
+        previous_ = current_;
+    }
+    
+} // namespace temgi
+
